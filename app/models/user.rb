f1dcfd3731d56c501e61751eff04d79c8826388a
gowthamk <gowtham@purdue.edu>
2016-06-30 15:10:26 -0400

    User creation stress test.

---
+++ app/models/user.rb
@@ -0,0 +1,61 @@
+class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  # GK: All users are admins.
+  def admin?
+    true
+  end
+
+  # GK: First, We print user name along with the micropost. To avoid
+  # "N+1 queries" problem, we eagerly load post.user. Second, the
+  # following method call results in two SQL queries. They need to be
+  # wrapped inside a transaction to avoid showing "Nil" in place of
+  # user name. Third, we need to either make transaction execute
+  # lazily, or make the two queries execute eagerly. I don't know how
+  # to do the former, so I did later using `load`.
+  def feed
+    self.transaction do
+      Micropost.includes(:user).from_users_followed_by(self).load
+    end
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
+end
